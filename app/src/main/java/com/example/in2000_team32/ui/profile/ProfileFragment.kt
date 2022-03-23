2d94e756f490eff246f2e68ec6b07b8832812f87
ErikAnkerKilbergSkallevold <42532920+ErikAnkerKilbergSkallevold@users.noreply.github.com>
2022-03-24 00:33:52 +0100

    La til fargevelger

--- app/src/main/java/com/example/in2000_team32/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/in2000_team32/ui/profile/ProfileFragment.kt
@@ -5,17 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.in2000_team32.R
 import com.example.in2000_team32.databinding.FragmentProfileBinding
+import dev.sasikanth.colorsheet.ColorSheet
+
+
 
 class ProfileFragment : Fragment() {
+
+    companion object {
+        private const val COLOR_SELECTED = "selectedColor"
+        private const val NO_COLOR_OPTION = "noColorOption"
+    }
 
     private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private var selectedColor: Int = ColorSheet.NO_COLOR
+    private var noColorOption = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +41,22 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val colors = resources.getIntArray(R.array.colors)
+
+
+        binding.editSkin.setOnClickListener {
+            ColorSheet().cornerRadius(8)
+                .colorPicker(
+                    colors = colors,
+                    noColorOption = noColorOption,
+                    selectedColor = selectedColor,
+                    listener = { color ->
+                        selectedColor = color
+                        setColor(selectedColor)
+                    })
+                .show(getParentFragmentManager())
+        }
+
         val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
@@ -35,8 +64,24 @@ class ProfileFragment : Fragment() {
         return root
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COLOR_SELECTED, selectedColor)
+        outState.putBoolean(NO_COLOR_OPTION, noColorOption)
+    }
+
+    private fun setColor(@ColorInt color: Int) {
+        if (color != ColorSheet.NO_COLOR) {
+            binding.constraintLayout1.setBackgroundColor(color)
+        } else {
+            binding.constraintLayout1.setBackgroundColor(R.drawable.bg_gradient)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
