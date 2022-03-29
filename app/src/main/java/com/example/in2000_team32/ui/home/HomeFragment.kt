df87d965d91457d1c098d1e1c0b551aa0b25d6a7
ErikAnkerKilbergSkallevold <erik.skallevold@gmail.com>
2022-03-29 12:25:35 +0200

    Keyboard er bedre

--- app/src/main/java/com/example/in2000_team32/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/in2000_team32/ui/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.in2000_team32.ui.home
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.in2000_team32.R
 import com.example.in2000_team32.databinding.FragmentHomeBinding
@@ -68,6 +72,8 @@ class HomeFragment : Fragment() {
         show = true
         binding.searchLayout1.animate().translationY(0F)
         binding.searchButton.setBackgroundResource(R.drawable.ic_baseline_close_24)
+        binding.EditTextAddress.requestFocus()
+        activity?.let { showKeyboard(it) }
     }
 
     fun hideSearch(){
@@ -75,7 +81,31 @@ class HomeFragment : Fragment() {
         show = false
         binding.searchLayout1.animate().translationY(searchDistance)
         binding.searchButton.setBackgroundResource(R.drawable.ic_baseline_search_24)
+        hideKeyboard()
     }
+
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun showKeyboard(activity: FragmentActivity) {
+        val inputMethodManager =
+            activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.toggleSoftInputFromWindow(
+            activity.currentFocus!!.windowToken,
+            InputMethodManager.SHOW_FORCED,
+            0
+        )
+    }
+
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
