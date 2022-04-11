cbf1319bfc9329ca4816380bfc07a286e60dd2a2
ErikAnkerKilbergSkallevold <erik.skallevold@gmail.com>
2022-04-11 21:39:05 +0200

    fikser mer på profilside

--- app/src/main/java/com/example/in2000_team32/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/in2000_team32/ui/profile/ProfileViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.in2000_team32.ui.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
+var getCurrent
+
 class ProfileViewModel : ViewModel() {
-    
+    private val _date = MutableLiveData<String>().apply {
+        value = currentDate
+    }
+    val dateText: LiveData<String> = _date
 }
