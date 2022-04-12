9885f109a72bed33c6c0a2383de45c6eba97e357
lukamomc <luka_momcilovic@hotmail.com>
2022-04-13 01:40:45 +0200

    netowork, gps provider fallback + alertdialog gps ved manglende gps/network + toasts og wifi av/på greier

--- app/src/main/java/com/example/in2000_team32/MainActivity.kt
+++ app/src/main/java/com/example/in2000_team32/MainActivity.kt
@@ -1,33 +1,74 @@
 package com.example.in2000_team32
 
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.in2000_team32.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: androidx.navigation.NavController
+    var internetLost = false
+
+    fun isNetworkAvailable(): Boolean {
+        var currentActivity = this
+        var result = false
+        val cm = this.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (cm != null) {
+                val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
+                if (capabilities != null) {
+                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        result = true
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        result = true
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
+                        result = true
+                    }
+                }
+            }
+        } else {
+            if (cm != null) {
+                val activeNetwork = cm.activeNetworkInfo
+                if (activeNetwork != null) {
+                    // connected to the internet
+                    if (activeNetwork.type == ConnectivityManager.TYPE_WIFI) {
+                        result = true
+                    } else if (activeNetwork.type == ConnectivityManager.TYPE_MOBILE) {
+                        result = true
+                    } else if (activeNetwork.type == ConnectivityManager.TYPE_VPN) {
+                        result = true
+                    }
+                }
+            }
+        }
+        return result
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         getSupportActionBar()?.hide()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -37,6 +78,85 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        val progressBar = binding.progressBar3
+        progressBar.visibility = View.GONE
+        //Get fragment with id home_layout
+
+        //Listen for internet connection established or lost and print message
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                println("Internet is available")
+                runOnUiThread {
+                    //Internet is back bitch
+                    if(internetLost) {
+                        Toast.makeText(this@MainActivity, "Internet is back", Toast.LENGTH_SHORT).show()
+                        internetLost = false
+                        //Restart activity without transition animation to avoid weird animation when coming back from settings activity to main activity
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                println("Internet is lost")
+                internetLost = true
+                runOnUiThread {
+                    //If current fragment is home fragment
+                    if (navController.currentDestination?.id == R.id.navigation_home) {
+                        progressBar.visibility = View.VISIBLE
+                        //Make the home fragment visibiity VISIBLE
+                        navController.navigate(R.id.navigation_home)
+                    }
+                }
+            }
+        }
+        //Listens for internet connection established
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+
+        //Listen for fragment changes
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(!isNetworkAvailable()) {
+                when (destination.id) {
+                    R.id.navigation_home -> {
+                        //Check if internet is
+                        progressBar.visibility = View.VISIBLE
+                        println("Home")
+                    }
+                    R.id.navigation_profile -> {
+                        progressBar.visibility = View.GONE
+                        println("Profile")
+                    }
+                    R.id.navigation_map -> {
+                        progressBar.visibility = View.GONE
+                        println("Map")
+                    }
+                }
+            }
+            else{
+                progressBar.visibility = View.GONE
+            }
+        }
+
+        //Check if the user has internet connection and if not show a Toast message to the user and show the progress bar and hide the navigation bar and the bottom navigation bar and the toolbar
+        if(!isNetworkAvailable()){
+            Toast.makeText(this, "No FUCKING internet connection", Toast.LENGTH_LONG).show()
+        }
+        //If user has internet connection
+        else{
+            //Hide the progress bar and show the navigation bar and the bottom navigation bar and the toolbar
+            progressBar.visibility  = View.GONE
+
+            //Show toast that user has internet connection
+            Toast.makeText(this, "You have internet connection", Toast.LENGTH_LONG).show()
+
+        }
+
+
 
     }
 
