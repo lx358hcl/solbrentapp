b8800e97f2cbd5ad76e5ce4332d57a4badd3945b
ErikAnkerKilbergSkallevold <42532920+ErikAnkerKilbergSkallevold@users.noreply.github.com>
2022-04-17 16:02:24 +0200

    UVbar bugs fikset, Uvbar settes progmatisk riktigere, ryddet opp i HomeFragment.kt. Fikset på Profilsiden, innstillinger går nå ikke med resten av listen

--- app/src/main/java/com/example/in2000_team32/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/in2000_team32/ui/home/HomeFragment.kt
@@ -28,14 +28,13 @@ import java.util.*
 import kotlin.math.roundToInt
 
 
+@Suppress("DEPRECATION")
 class HomeFragment : Fragment() {
     var show = false
     private var _binding: FragmentHomeBinding? = null
     private val current: LocalDateTime = LocalDateTime.now()
     private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH", Locale.getDefault())
     private val formatted: Double = current.format(formatter).toDouble()
-
-    private var uvStyrke: Int = 11
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
@@ -51,8 +50,9 @@ class HomeFragment : Fragment() {
 
         val root: View = binding.root
 
-        val searchButton = binding.searchButton
 
+        //If som åpner og lukker søkefeltet
+        val searchButton = binding.searchButton
         searchButton.setOnClickListener {
             if (show) {
                 hideSearch()
@@ -60,13 +60,14 @@ class HomeFragment : Fragment() {
                 showSearch()
             }
         }
+        //End of if som åpner og lukker søkefeltet
 
 
 
 
         //Sett solkrem
-
         binding.imageViewSolkrem.setImageResource(R.drawable.solkrem_lang_50pluss)
+        //End of sett solkrem
 
         //Sjekk om det er darkmode eller ikke og sett været
         when (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
@@ -80,51 +81,11 @@ class HomeFragment : Fragment() {
                 binding.vaermeldingSky.setImageResource(R.drawable.tordenskyh)
             }
         }
+        //End of sjekk om det er darkmode eller ikke og sett været
 
         //Ser på klokken og bytter blobb
-
-        when (formatted) {
-            0.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            1.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            2.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            3.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            4.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            5.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            6.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.purple_blob)
-            7.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.purple_blob)
-            8.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.purple_blob)
-            9.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.green_blob)
-            10.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.green_blob)
-            11.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.green_blob)
-            12.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
-            13.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
-            14.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
-            15.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
-            16.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
-            17.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
-            18.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            19.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            20.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            21.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            22.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            23.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
-            else -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
-        }
-
-
-        val view: View = root.findViewById(R.id.imageViewUvPin)
-        val params = view.layoutParams as PercentRelativeLayout.LayoutParams
-// This will currently return null, if it was not constructed from XML.
-// This will currently return null, if it was not constructed from XML.
-        val info = params.percentLayoutInfo
-        info.startMarginPercent = 0.0818f
-        view.requestLayout()
-
-
-
-
-
-
+        settBlobb()
+        //End of se på klokken og bytter blobb
 
 
 
@@ -147,49 +108,8 @@ class HomeFragment : Fragment() {
             getGeoLocation(currentActivity)
         }
 
-        //Usikker på hva denne gjør
 
-        //Setter UV pin og tekst i detjalert view
-        fun setUvPin(f: Float) {
-            val view: View = root.findViewById(R.id.imageViewUvPin)
-            val params = view.layoutParams as PercentRelativeLayout.LayoutParams
-            val info = params.percentLayoutInfo
-            info.startMarginPercent = f
-            view.requestLayout()
-        }
 
-        fun setUvPinTekst(f: Float, i: Int) {
-            val view: View = root.findViewById(R.id.textViewUvPinTall)
-            val params = view.layoutParams as PercentRelativeLayout.LayoutParams
-            val info = params.percentLayoutInfo
-            info.startMarginPercent = f
-            binding.textViewUvPinTall.text = i.toString()
-            view.requestLayout()
-        }
-
-        fun setUvAlle(f: Float, i: Int) {
-            setUvPin(f)
-            setUvPinTekst(f, i)
-        }
-
-        fun setUvBar(uv: Int){
-            when(uv){
-                0 -> setUvAlle(0.0f, 0)
-                1 -> setUvAlle(0.0818f, 1)
-                2 -> setUvAlle(0.1636f, 2)
-                3 -> setUvAlle(0.2454f, 3)
-                4 -> setUvAlle(0.3272f, 4)
-                5 -> setUvAlle(0.4090f, 5)
-                6 -> setUvAlle(0.4909f, 6)
-                7 -> setUvAlle(0.5727f, 7)
-                8 -> setUvAlle(0.6545f, 8)
-                9 -> setUvAlle(0.7363f, 9)
-                10 -> setUvAlle(0.8181f, 10)
-                11 -> setUvAlle(0.9f, 11)
-            }
-        }
-
-        setUvBar(uvStyrke)
 
 
 
@@ -252,7 +172,7 @@ class HomeFragment : Fragment() {
         getActivity()?.let {
             homeViewModel.getUvData().observe(it) {
                 binding.textUvi.setText(it.toString() + " uvi")
-                uvStyrke = it.roundToInt()
+                setUvBar(it.roundToInt(), it)
             }
         }
         // Get weather message
@@ -303,6 +223,85 @@ class HomeFragment : Fragment() {
             }
         }
     }
+
+    //Setter UV pin og tekst i detjalert view
+    fun setUvPin(f: Float) {
+        val view: View = binding.imageViewUvPin
+        val params = view.layoutParams as PercentRelativeLayout.LayoutParams
+        val info = params.percentLayoutInfo
+        info.startMarginPercent = f
+        view.requestLayout()
+    }
+
+    fun setUvPinTekst(f: Float, d: Double) {
+        val view: View = binding.textViewUvPinTall
+        val params = view.layoutParams as PercentRelativeLayout.LayoutParams
+        val info = params.percentLayoutInfo
+
+        if(d >= 10.5){
+            info.startMarginPercent = f - 0.10f
+        } else {
+            info.startMarginPercent = f - 0.05f
+        }
+        binding.textViewUvPinTall.text = d.toString()
+        view.requestLayout()
+    }
+
+    fun setUvAlle(f: Float, d: Double) {
+        setUvPin(f)
+        setUvPinTekst(f, d)
+    }
+
+    fun setUvBar(uv: Int, d: Double){
+        when(uv){
+            0 -> setUvAlle(0.0f, d)
+            1 -> setUvAlle(0.0818f, d)
+            2 -> setUvAlle(0.1636f, d)
+            3 -> setUvAlle(0.2454f, d)
+            4 -> setUvAlle(0.3272f, d)
+            5 -> setUvAlle(0.4090f, d)
+            6 -> setUvAlle(0.4909f, d)
+            7 -> setUvAlle(0.5727f, d)
+            8 -> setUvAlle(0.6545f, d)
+            9 -> setUvAlle(0.7363f, d)
+            10 -> setUvAlle(0.8181f, d)
+            11 -> setUvAlle(0.9f, d)
+        }
+    }
+    //End of set UV pin og UV tekst
+
+    //Ser på klokken og bytter blobb
+    fun settBlobb(){
+        when (formatted) {
+            0.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            1.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            2.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            3.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            4.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            5.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            6.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.purple_blob)
+            7.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.purple_blob)
+            8.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.purple_blob)
+            9.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.green_blob)
+            10.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.green_blob)
+            11.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.green_blob)
+            12.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
+            13.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
+            14.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
+            15.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
+            16.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
+            17.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
+            18.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            19.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            20.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            21.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            22.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            23.0 -> binding.vaermeldingBlob.setImageResource(R.drawable.pink_blob)
+            else -> binding.vaermeldingBlob.setImageResource(R.drawable.blue_blob)
+        }
+    }
+    //End of se på klokken og bytter blobb
+
 
 
     override fun onDestroyView() {
